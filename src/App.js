d086c0124de8b627edc0e43cfdb41b4ace1a5718
“mtbishmam” <mtbishmam@gmail.com>
2024-04-02 12:45:57 +0600

    Deleted src and recreated it

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
 
-// function Person(props) {
-//   return (
-//     <>
-//       <h1>Name: {props.name || "John"}</h1>
-//       <h1>Last Name: {props.lastName || "Doe"}</h1>
-//       <h1>Age: {props.age || 30}</h1>
-//     </>
-//   );
-// }
-
-function App() {
-  let [counter, setCounter] = useState(0);
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
