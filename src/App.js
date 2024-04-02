33ecdbdbd1486e2658dd37bcc1b743c93e3c8d5a
“mtbishmam” <mtbishmam@gmail.com>
2024-04-02 13:15:09 +0600

    Implemented OMDB API key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=4a78df0b";
 
 const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+  };
+
+  useEffect(() => {
+    searchMovie("Naruto");
+  }, []);
+
   return <div>App</div>;
 };
 
